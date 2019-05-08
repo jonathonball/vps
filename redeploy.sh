6e6f8447091b32803acb77fc43c97aa4bea421ec
Jon Ball <jonathon.ball@gmail.com>
2019-05-07 22:29:36 -0400

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook delete-websites.yaml && ansible-playbook server.yaml --tags web-application;
+ansible-playbook delete-websites.yaml && ansible-playbook server.yaml --tags app;
 
